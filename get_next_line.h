f0e3482ef53f98684ffc3e16784d1c0b29b9ee17
Martijn Van ginkel <mvan-gin@f0r3s26.codam.nl>
2019-11-21 15:52:39 +0100

    Removed libraries from .h file

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: mvan-gin <mvan-gin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/11/19 13:07:10 by mvan-gin       #+#    #+#                */
-/*   Updated: 2019/11/20 13:56:41 by mvan-gin      ########   odam.nl         */
+/*   Updated: 2019/11/21 15:51:48 by mvan-gin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,6 @@
 
 # include <stdlib.h>
 # include <unistd.h>
-# include <fcntl.h>
-# include <stdio.h>
 
 int		enough_for_one_line(char *line);
 char	*read_bufsize(int fd, int *end_of_file);
